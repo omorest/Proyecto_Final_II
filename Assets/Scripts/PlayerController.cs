677a59433347e6c9c85a7869322585be7e3a78b0
Miguel Angel <j2mc1410@gmail.com>
2021-01-04 15:08:44 +0000

    Movimiento implementado

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+  private float xAxis, zAxis;
+  public float speed;
+
+  private void Start() {
+    speed = 50f;
+  }
+
+  private void Update() {
+    Walk();
+  }
+
+  private void Walk() {
+    //xAxis = Input.GetAxis("Horizontal");
+    //zAxis = Input.GetAxis("Vertical");
+
+    xAxis = 0.1f;
+    zAxis = 0.1f;
+
+    Vector3 movement = new Vector3(xAxis, 0, zAxis);
+    transform.Translate(movement * speed * Time.deltaTime);
+  }
+}
