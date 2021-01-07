db2f78f4b15b627c4b35af1dc0bab165076012e0
Miguel Angel <j2mc1410@gmail.com>
2021-01-07 17:18:23 +0000

    Jugador con física

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
   private float xAxis, zAxis;
-  
+  private Vector3 movement;
+  private CharacterController player;
+
   [Range (10, 50)]
   public float speed;
 
   private void Start() {
-    speed = 50f;
+    player = GetComponent<CharacterController>();
+    speed = 30f;
   }
 
   private void Update() {
-    Walk();
-  }
-
-  private void Walk() {
     xAxis = Input.GetAxis("Horizontal");
     zAxis = Input.GetAxis("Vertical");
 
     Vector3 absoluteMovement = new Vector3(xAxis, 0.0f, zAxis);
     Quaternion directionCamera = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0);
-    Vector3 relativeMovement = directionCamera * absoluteMovement;
-
-    transform.Translate(relativeMovement * speed * Time.deltaTime);
+    movement = directionCamera * absoluteMovement;
   }
+
+  private void FixedUpdate() {
+    player.Move(movement * speed * Time.deltaTime);
+  }  
 }
