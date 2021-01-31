b5a446766ccd5b84c0a311af1d6cdd983ea78dfa
Miguel Angel <j2mc1410@gmail.com>
2021-01-31 00:12:46 +0000

    Barra de poder

---
+++ Assets/Scripts/Canvas/ShowPlayerPower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowPlayerPower : MonoBehaviour {
+  public Image power;
+  private vidaJugador player;
+  
+  private void Start() {
+    player = GameObject.FindWithTag("PlayerTag").GetComponent<vidaJugador>();
+  }
+
+  private void Update() {
+    power.fillAmount = player.GetDañoBala() / 100.0f;
+  }
+}
